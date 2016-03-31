03ea164bb381a6b6e7811488eec135cebdafd6dc
Kevin Porras <kporras07@gmail.com>
2016-03-30 22:24:35 -0600

    Initial Commit

---
+++ scripts/manatiblog_local_install.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+DRUSH="./vendor/bin/drush"
+SITE_ALIAS="@manatiblog.manatiblog.dev"
+$DRUSH $SITE_ALIAS cc drush
+echo "Installing..."
+if [ -f ./files/config/sync/core.extension.yml ] ; then $DRUSH $SITE_ALIAS si manatiblog --config-dir=sites/default/files/config/sync -y ; else $DRUSH $SITE_ALIAS si manatiblog -y ; fi
+#echo "Setting master scope..."
+#$DRUSH $SITE_ALIAS master-set-current-scope local
+#echo "Executing master..."
+#$DRUSH $SITE_ALIAS master-execute -y
+echo "Importing config..."
+$DRUSH $SITE_ALIAS cim -y
+echo "Cleaning cache..."
+$DRUSH $SITE_ALIAS cr
---
+++ wercker/manatiblog.aliases.drushrc.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * @file
+ * Wercker Drush Aliases.
+ */
+
+$aliases['manatiblog.dev'] = array(
+  'uri' => 'manatiblog.dev',
+  'root' => '/pipeline/source/build',
+);
+
