89d4ff9cafdf5fd76ba6e5170d407117b4529544
Kevin Porras <kporras07@gmail.com>
2016-04-04 17:49:57 -0600

    Change some stuff to test if it work.s

--- scripts/manatiblog_local_install.sh
+++ scripts/manatiblog_local_install.sh
@@ -3,12 +3,13 @@ DRUSH="./vendor/bin/drush"
 SITE_ALIAS="@manatiblog.manatiblog.dev"
 $DRUSH $SITE_ALIAS cc drush
 echo "Installing..."
-if [ -f ./files/config/sync/core.extension.yml ] ; then $DRUSH $SITE_ALIAS si manatiblog --account-pass=admin --config-dir=sites/default/files/config/sync -y ; else $DRUSH $SITE_ALIAS si manatiblog --account-pass=admin -y ; fi
+if [ -f ./files/config/sync/core.extension.yml ] ; then $DRUSH $SITE_ALIAS si standard --account-pass=admin --config-dir=sites/default/files/config/sync -y ; else $DRUSH $SITE_ALIAS si standard --account-pass=admin -y ; fi
 #echo "Setting master scope..."
 #$DRUSH $SITE_ALIAS master-set-current-scope local
 #echo "Executing master..."
 #$DRUSH $SITE_ALIAS master-execute -y
-echo "Importing config..."
-$DRUSH $SITE_ALIAS cim -y
+echo "Importinc configuration..."
+drush ev '\Drupal::entityManager()->getStorage("shortcut_set")->load("default")->delete();'
+drush cim -y
 echo "Cleaning cache..."
 $DRUSH $SITE_ALIAS cr
