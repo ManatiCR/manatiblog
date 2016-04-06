9c39cb639a7f5c9e649a60ab1cc76e54826be75d
Kevin Porras <kporras07@gmail.com>
2016-04-05 23:28:11 -0600

    Update manati custom formatters.

--- modules/custom/manati_custom_formatters/src/Plugin/Field/FieldFormatter/RawImagePath.php
+++ modules/custom/manati_custom_formatters/src/Plugin/Field/FieldFormatter/RawImagePath.php
@@ -10,6 +10,7 @@ namespace Drupal\manati_custom_formatters\Plugin\Field\FieldFormatter;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\image\Plugin\Field\FieldFormatter\ImageFormatter;
 use Drupal\Core\Field\FieldItemListInterface;
+use Drupal\Core\Cache\Cache;
 
 /**
  * RawImagePath Field Formatter.
@@ -43,36 +44,39 @@ class RawImagePath extends ImageFormatter {
   /**
    * {@inheritdoc}
    */
-  public function viewElements(FieldItemListInterface $items) {
+  public function viewElements(FieldItemListInterface $items, $langcode) {
     $elements = array();
+    $files = $this->getEntitiesToView($items, $langcode);
+
+    // Early opt-out if the field is empty.
+    if (empty($files)) {
+      return $elements;
+    }
+
     $image_style_setting = $this->getSetting('image_style');
-    // Collect cache tags to be added for each item in the field.
-    $cache_tags = array();
-    if (!empty($image_style_setting)) {
-      $image_style = entity_load('image_style', $image_style_setting);
-      $cache_tags = $image_style->getCacheTags();
+    $image_style = $this->imageStyleStorage->load($image_style_setting);
+    $cache_tags = $image_style->getCacheTags();
+
+    foreach ($files as $delta => $file) {
+      $image_uri = $file->getFileUri();
+      $cache_tags = Cache::mergeTags($cache_tags, $file->getCacheTags());
+      $url = $image_style->buildUrl($image_uri, TRUE);
+
+      // Extract field item attributes for the theme function, and unset them
+      // from the $item so that the field template does not re-render them.
+      $item = $file->_referringItem;
+      $item_attributes = $item->_attributes;
+      unset($item->_attributes);
+
+      $elements[$delta] = array(
+        '#type' => 'markup',
+        '#markup' => $url,
+        '#cache' => array(
+          'tags' => $cache_tags,
+        ),
+      );
     }
-    foreach ($items as $delta => $item) {
-      if ($item->entity) {
-        $image_uri = $item->entity->getFileUri();
-        if (!empty($image_style)) {
-          $path = $image_style->buildUrl($image_uri);
-          if (!file_exists($path)) {
-            $image_style->createDerivative($image_uri, $path);
-          }
-        }
-        else {
-          $path = file_create_url($image_uri);
-        }
-        $elements[$delta] = array(
-          '#type' => 'markup',
-          '#markup' => $path,
-          '#cache' => array(
-            'tags' => $cache_tags,
-          ),
-        );
-      }
-    }
+
     return $elements;
   }
 
